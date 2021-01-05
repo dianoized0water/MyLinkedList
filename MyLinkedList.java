d7707a33239527f266213eef8d5eb2eb0e214a2e
diana_yang <dyang20@stuy.edu>
2021-01-05 13:14:34 -0500

    initial commit

---
+++ MyLinkedList.java
@@ -0,0 +1,20 @@
+public class MyLinkedList{
+  private int size;
+  private Node start, end;
+
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String value){
+
+  }
+
+  public boolean add(int index, String value){
+
+  }
+
+  public String toString(){
+    
+  }
+}
