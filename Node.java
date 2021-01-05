818f2f86cb6ed8f00f0a64f9934ca0a02e26dc07
diana_yang <dyang20@stuy.edu>
2021-01-05 13:23:09 -0500

    added constructor as well as get and set methods

--- Node.java
+++ Node.java
@@ -1,4 +1,34 @@
 public class Node{
   private String data;
   private Node next, prev;
+
+  public Node(int value, Node next, Node prev){
+    data = value;
+    this.next = next;
+    this.prev = prev;
+  }
+
+  public int getValue(){
+    return data;
+  }
+
+  public Node getNext(){
+    return next;
+  }
+
+  public Node getPrev(){
+    return prev;
+  }
+
+  public void setNext(Node newNode){
+    next = newNode;
+  }
+
+  public void setValue(int newValue){
+    data = newValue;
+  }
+
+  public void setPrev(Node newNode){
+    prev = newNode;
+  }
 }
